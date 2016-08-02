0b531a3f83425e319a8f2daff2551c627aaa11c0
chedva <chedvaberdy@gmail.com>
2016-08-02 14:55:42 -0400

    cleaned up rock, paper, scissor

--- Day2/RockScissorPaper.js
+++ Day2/RockScissorPaper.js
@@ -23,36 +23,72 @@ function DomAlert(msg){
 
 }
 
+function ConvertValues(entry){
+	
+	
+	 var convertValue;
+	//alert(!isNaN(parseFloat(entry)) && isFinite(entry);)
+	 if (entry===0 || entry === 1 || entry===2){
+	// 	//Convert computer value to string
+		
+		if (entry===0){
+			convertValue="rock";
+		}else if (entry===1){
+			convertValue="scissor";
+		}else if (entry===2){
+			convertValue="paper";
+		}
+	
+
+	}else{
+			//eval users input and convert to #
+		
+		userTurn=entry.toUpperCase();
+		
+		if (userTurn==="R" ){
+			convertValue=0;
+		}else if (userTurn==="S"){
+			convertValue=1;
+		}else if (userTurn==="P"){
+			convertValue=2;
+		}
+		
 
 
-function ComputeWinner(userTurn){
+	 }
+	 return convertValue;
+}
 
 
-	//eval users input and convert to #
-	var userValue;
-	userTurn=userTurn.toUpperCase();
-	if (userTurn==="R" ){
-		userValue=0;
-	}else if (userTurn==="S"){
-		userValue=1;
-	}else if (userTurn==="P"){
-		userValue=2;
-	}
-
-	//get computer generated value
-	var computerValue=Math.floor(Math.random()*3);
-
-	//Convert computer value to string
-	var computerStringValue;
-	if (computerValue===0){
-		computerStringValue="rock";
-	}else if (computerValue===1){
-		computerStringValue="scissor";
-	}else if (computerValue===2){
-		computerStringValue="paper";
-	}
+//function ComputeWinner(userTurn){
 
 
+	// //eval users input and convert to #
+	// var userValue;
+	// userTurn=userTurn.toUpperCase();
+	// if (userTurn==="R" ){
+	// 	userValue=0;
+	// }else if (userTurn==="S"){
+	// 	userValue=1;
+	// }else if (userTurn==="P"){
+	// 	userValue=2;
+	// }
+
+	// //get computer generated value
+	// var computerValue=Math.floor(Math.random()*3);
+
+	// //Convert computer value to string
+	// var computerStringValue;
+	// if (computerValue===0){
+	// 	computerStringValue="rock";
+	// }else if (computerValue===1){
+	// 	computerStringValue="scissor";
+	// }else if (computerValue===2){
+	// 	computerStringValue="paper";
+	// }
+
+
+function ComputeWinner(userValue, computerValue){
 
 	//evaluate winner
 	if (userValue===computerValue){
@@ -71,6 +107,8 @@ function ComputeWinner(userTurn){
 DomPrompt("Enter your choice of (R)ock, (S)cissor, or (P)aper");
 //get text in text box and eval if enter clicked and then get value
 var userTurn;
+var useValue;
+var computerStringValue;
 
 var userInput = document.getElementById('txtPrompt');
 
@@ -86,7 +124,15 @@ var userInput = document.getElementById('txtPrompt');
 		//userTurn=userTurn.toUpperCase(); //to allow lower case and upper case of r, s, p
 
 		if (userTurn==="R" || userTurn ==="S" || userTurn==="P" || userTurn==="r" || userTurn === "s" || userTurn==="p") {
- 			ComputeWinner (userTurn);
+			userValue=ConvertValues(userTurn);
+
+			
+			// //get computer generated value
+			var computerValue=Math.floor(Math.random()*3);
+
+			computerStringValue=ConvertValues(computerValue);
+
+			ComputeWinner (userValue,computerValue);
  			
 
 
@@ -96,55 +142,5 @@ var userInput = document.getElementById('txtPrompt');
 
 }
 
-
-
-
-
-// do {
-// 	var userTurn = prompt ("Enter your choice of (R)ock, (S)cissor, or (P)aper");
-// 	userTurn=userTurn.toUpperCase(); //to allow lower case and upper case of r, s, p
-
-// 	if (userTurn!="R" && userTurn !="S" && userTurn!="P") {
-// 		alert ("Invalid user entry try again");
-// 	}
-	
-// }while (userTurn!="R" && userTurn !="S" && userTurn!="P");
-
-
-// //eval users input and convert to #
-// var userValue;
-// if (userTurn==="R"){
-// 	userValue=0;
-// }else if (userTurn==="S"){
-// 	userValue=1;
-// }else if (userTurn==="P"){
-// 	userValue=2;
-// }
-
-// //get computer generated value
-// var computerValue=Math.floor(Math.random()*3);
-
-// //Convert computer value to string
-// var computerStringValue;
-// if (computerValue===0){
-// 	computerStringValue="rock";
-// }else if (computerValue===1){
-// 	computerStringValue="scissor";
-// }else if (computerValue===2){
-// 	computerStringValue="paper";
-// }
-
-
-
-// //evaluate winner
-// if (userValue===computerValue){
-// 	alert("Computer chose " + computerStringValue + ". Tie game!");
-
-// }else if(((userValue===0 || userValue===1)  && ((userValue+1)===computerValue)) || (userValue===2 && computerValue==0)){
-// 	alert ("Computer chose " + computerStringValue + ". You are the winner!");
-
-// }else if (((computerValue===0 || computerValue===1) && ((userValue-1)===computerValue)) || (computerValue===2 && userValue==0)){
-// 	alert ("Computer chose " + computerStringValue + ". Sorry, you lose. Computer wins!");
-// }
 
 
---
+++ Day2/SumValues.js
@@ -0,0 +1,14 @@
+function sum(){
+	var paramLen=arguments.length;
+	var tempSum=0;
+
+	
+	for (var x=0;x<paramLen;x++){
+		tempSum+=arguments[x];
+		
+	}
+
+	return tempSum;
+}
+
+alert(sum (1,2,3,4,5,6));
